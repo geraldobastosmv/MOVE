555b08000fadc6440094cc94160bab02a8ad4158
Luiz Motta <luizdamm@gmail.com>
2025-09-02 18:08:23 -0300

    Adicionando outras sections ao Home

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,11 +1,15 @@
 import HeroBooking from "./HeroBooking/";
 import HeroCard from "./HeroCards";
+import HeroSecurity from "./HeroSecurity";
+import HeroSteps from "./HeroSteps";
 
 export default function Home() {
   return (
     <>
       <HeroBooking />
       <HeroCard />
+      <HeroSecurity />
+      <HeroSteps/>
     </>
   );
 }
