c7ccefbd111e476bcf6bc6519e7de69f278de4e5
Luiz Motta <luizdamm@gmail.com>
2025-09-03 16:46:44 -0300

    Adicionando hero de comentarios

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import HeroBooking from "./HeroBooking/";
 import HeroCard from "./HeroCards";
 import HeroSecurity from "./HeroSecurity";
 import HeroSteps from "./HeroSteps";
+import HeroComents from "./HeroComents";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <HeroCard />
       <HeroSecurity />
       <HeroSteps/>
+      <HeroComents/>
     </>
   );
 }
