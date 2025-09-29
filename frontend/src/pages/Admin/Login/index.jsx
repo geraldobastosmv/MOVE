538a9dc588959724bd81f21400733c4fd9292e47
Luiz Motta <luizdamm@gmail.com>
2025-09-29 16:28:23 -0300

    Login.jsx | Layout para mobile corrigido

--- frontend/src/pages/Admin/Login/index.jsx
+++ frontend/src/pages/Admin/Login/index.jsx
@@ -9,7 +9,7 @@ const Login = () => {
   }
 
   return (
-    <section className="h-130 flex items-center justify-center mb-45">
+    <section className="h-130 flex items-center justify-center mb-45 px-(--sdp)">
       <div className="flex flex-col gap-6 items-center justify-center w-full max-w-md">
         <h1 className="font-bold text-3xl mb-2">Entrar</h1>
         <form className="flex flex-col bg-white p-10 rounded-2xl border border-gray-200 shadow-xl w-full gap-5">
